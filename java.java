b9784bb55323b2c719f8ddd5f7dfc77bbc0531a7
chae woon <javaall123@naver.com>
2022-01-18 15:41:58 +0900

    조건문 예제1

---
+++ java.java
@@ -0,0 +1,11 @@
+class java {
+    public static void main(String[] args){
+        //조건문 예제1
+        int a = 5;
+        if(a == 5) {
+            System.out.println("a는 5입니다.");
+        } else {
+            System.out.println("a는 5가 아닙니다.");
+        }
+    }
+}
