7b8678f8de392842f5f019fdb601259bdbab6d04
chae woon <javaall123@naver.com>
2022-01-18 15:44:56 +0900

    for문 예제

--- java.java
+++ java.java
@@ -1,11 +1,8 @@
 class java {
     public static void main(String[] args){
-        //조건문 예제1
-        int a = 5;
-        if(a == 5) {
-            System.out.println("a는 5입니다.");
-        } else {
-            System.out.println("a는 5가 아닙니다.");
+        int i = 0;
+        for(i=0;i<10;i++) {
+            System.out.println("Count " + (i+1));
         }
     }
 }
