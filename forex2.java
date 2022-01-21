1b8f135ec8a1bab054f56406aec2fac389b586cd
chae woon <javaall123@naver.com>
2022-01-21 16:18:26 +0900

    for문 문제 2

---
+++ forex2.java
@@ -0,0 +1,9 @@
+public class forex2 {
+    public static void main(String[] args) {
+        int original = 5;
+        int i;
+        for(i = 1; i < 10; i++) {
+            System.out.printf("%d x %d = %d\n", original, i, original * i);
+        }
+    }
+}
