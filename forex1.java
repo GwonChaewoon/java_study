6061980711a1bfaa665dbee39518be426e99604b
chae woon <javaall123@naver.com>
2022-01-21 16:10:56 +0900

    for문 문제 1

---
+++ forex1.java
@@ -0,0 +1,10 @@
+public class forex1 {
+    public static void main(String[] args) {
+        int i;
+        int total = 1;
+        for(i = 1; i <= 10; i++) {
+            total *= i;
+        }
+        System.out.println(total);
+    }
+}
