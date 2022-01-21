708c7665d79a77563ae40cbeccf6f235d915c97b
chae woon <javaall123@naver.com>
2022-01-21 14:39:06 +0900

    연산자 문제 1번

---
+++ Opex1.java
@@ -0,0 +1,10 @@
+class Opex1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        num1 = num2 = num3;
+        System.out.printf("%d %d %d", num1, num2, num3);
+    }
+}
