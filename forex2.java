07d93bb4712f58f30afc93d752644bca9286fe1e
chae woon <javaall123@naver.com>
2022-01-21 16:23:08 +0900

    break문

--- forex2.java
+++
@@ -1,9 +0,0 @@
-public class forex2 {
-    public static void main(String[] args) {
-        int original = 5;
-        int i;
-        for(i = 1; i < 10; i++) {
-            System.out.printf("%d x %d = %d\n", original, i, original * i);
-        }
-    }
-}
