4c2cf26a4af7bb9ba3ba952e991f572b9623ee34
chae woon <javaall123@naver.com>
2022-01-21 15:34:11 +0900

    if문과 if ~ else문 예제

---
+++ IFReit.java
@@ -0,0 +1,11 @@
+public class IFReit {
+    public static void main(String[] args) {
+        int num = 120;
+        if(num > 0 && (num % 2) == 0) {
+            System.out.println("양수이면서 짝수");
+        }
+        else {
+            System.out.println("홀수");
+        }
+    }
+}
