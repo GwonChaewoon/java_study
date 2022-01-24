98cbdd9583614c3e46288d2bdd6cf18ce68a7adb
chae woon <javaall123@naver.com>
2022-01-24 14:05:07 +0900

    (수정)

--- BreakPoint.java
+++ BreakPoint.java
@@ -1,10 +1,10 @@
 public class BreakPoint {
     public static void main(String[] args) {
-        for(int i = 1; i < 10; i++) {
+outer: for(int i = 1; i < 10; i++) {
             for(int j = 1; j < 10; j ++) {
                 if((i * j) == 72) {
                     System.out.println(i + " x " + j + " = " + i*j);
-                    break;
+                    break outer;
                 }
             }
         }
