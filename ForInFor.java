edfccf383c392388657688a80b848b93050c9c87
chae woon <javaall123@naver.com>
2022-01-24 13:45:58 +0900

    For문 중첩(수정)

--- ForInFor.java
+++ ForInFor.java
@@ -3,9 +3,9 @@ class ForInFor {
         for(int i = 0; i < 3; i++) {
             System.out.println("---------------------");
             for(int j = 0; j < 3; j++) {
-                System.out.println("[" + i + ", " + j + "] ");
+                System.out.print("[" + i + ", " + j + "] ");
             }
-            System.out.println("\n");
+            System.out.print("\n");
         }
     }
 }
