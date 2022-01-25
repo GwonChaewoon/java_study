dd245545c15bba4872633b40d42987fced6d97fc
chae woon <javaall123@naver.com>
2022-01-25 20:37:14 +0900

    생성자(Constructor)

--- BankAccountConstructor.java
+++ BankAccountConstructor.java
@@ -1,10 +1,9 @@
 class BankAccount {
-    String accNumber;    //계좌번호
-    String ssNumber;     //주민번호
-    int balance = 0;     //예금 잔액
+    String accNumber;
+    String ssNumber;
+    int balance;
 
-    //초기화를 위한 메소드
-    public void initAccount(String acc, String ss, int bal) {
+    public BankAccount(String acc, String ss, int bal) {
         accNumber = acc;
         ssNumber = ss;
         balance = bal;
@@ -20,18 +19,15 @@ class BankAccount {
     public int checkMyBalance() {
         System.out.println("계좌번호: " + accNumber);
         System.out.println("주민번호: " + ssNumber);
-        System.out.println("잔 액: " + balance + '\n');
+        System.out.println("잔 액: " + balance);
         return balance;
     }
 }
 
-public class BankAccountUniID {
+public class BankAccountConstructor {
     public static void main(String[] args) {
-        BankAccount yoon = new BankAccount();
-        yoon.initAccount("12-34-89", "990990-9090990", 10000);
-
-        BankAccount park = new BankAccount();
-        park.initAccount("33-55-09", "770088-5959007", 10000);
+        BankAccount yoon = new BankAccount("12-34-89", "990990-9090990", 10000);
+        BankAccount park = new BankAccount("33-55-09", "770088-5959007", 10000);
 
         yoon.deposit(5000);
         park.deposit(3000);
