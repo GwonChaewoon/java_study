b248213ff123a895ad0fd727654eb699c9083abc
chae woon <javaall123@naver.com>
2022-02-05 23:04:40 +0900

    Scanner 클래스의 키보드 적용

--- ScanningString.java
+++ ScanningString.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class ScanningString {
     public static void main(String[] args) {
-        String source = "1 3 5";
-        Scanner sc = new Scanner(source);
+        Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
