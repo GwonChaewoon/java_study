f2f7b9c5c413de2ad085e48aae51887cc26a3f98
chae woon <javaall123@naver.com>
2022-02-12 23:45:26 +0900

    Exception을 상속하는 예외 클래스의 예외처리 Exception을 상속하는 예외 클래스 중에서 비교적 빨리 접하게 될 클래스는 다음과 같다. java.io.IOException

--- NullPointer.java
+++
@@ -1,7 +0,0 @@
-public class NullPointer {
-    public static void main(String[] args) {
-        String str = null;
-        System.out.println(str);    // null 출력
-        int len = str.length();     // Exception!
-    }
-}
