4a3f99156f0f089f927f90db22673277c6e6ac86
chae woon <javaall123@naver.com>
2022-02-12 22:56:39 +0900

    예외의 처리를 위한 try ~ catch try ~ catch문은 try 영역과 catch 영역으로 구분이 되는데, 이 둘은 하나의 문장이므로 항상 연결되어 있어야 한다. 그리고 이 문장이 동작하는 방식은 다음과 같다. "try 영역에서 발생한 예외 상황을 catch 영역에서 처리한다."

--- ExceptionCase.java
+++ ExceptionCase.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class ExceptionCase {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        System.out.print("a/b...a? ");
-        int n1 = kb.nextInt();    // int형 정수 입력
-        System.out.print("a/b...b? ");
-        int n2 = kb.nextInt();    // int형 정수 입력
-        System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);
+
+        try {
+            System.out.print("a/b...a? ");
+            int n1 = kb.nextInt();
+            System.out.print("a/b...b? ");
+            int n2 = kb.nextInt();
+            System.out.printf("%d / %d = %d \n", n1, n2, n1 / n2);  //예외 발생 지점
+        }
+        catch(ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+        
         System.out.println("Good bye~~!");
     }
 }
