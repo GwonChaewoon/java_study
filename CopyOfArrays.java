f988d19c3ca6ba1dd5126e39cbafcafb61c196d9
chae woon <javaall123@naver.com>
2022-02-16 15:49:05 +0900

    배열의 복사(2)

--- CopyOfArrays.java
+++ CopyOfArrays.java
@@ -1,20 +1,12 @@
-import java.util.Arrays;
-
 public class CopyOfArrays {
     public static void main(String[] args) {
-        double[] arOrg = {1.1, 2.2, 3.3, 4.4, 5.5};
+        double[] org = {1.1, 2.2, 3.3, 4.4, 5.5};
+        double[] cpy = new double[3];
+        
+        //배열 org의 인덱스 1에서 배열 cpy 인덱스 0으로 세 개의 요소 복사
+        System.arraycopy(org, 1, cpy, 0, 3);
 
-        // 배열 전체를 복사
-        double[] arCpy1 = Arrays.copyOf(arOrg, arOrg.length);
-
-        // 세번째 요소까지만 복사
-        double[] arCpy2 = Arrays.copyOf(arOrg, 3);
-
-        for(double d : arCpy1)
-            System.out.print(d + "\t");
-        System.out.println();
-
-        for(double d : arCpy2)
+        for(double d : cpy)
             System.out.print(d + "\t");
         System.out.println();
     }
