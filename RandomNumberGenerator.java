034f5d47ce87a58ff7d30a0c4cae898c125f84a0
chae woon <javaall123@naver.com>
2022-02-16 15:18:08 +0900

    씨드(Seed) 기반의 난수 생성(2)

--- RandomNumberGenerator.java
+++ RandomNumberGenerator.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class RandomNumberGenerator {
     public static void main(String[] args) {
-        Random rand = new Random(12);
+        Random rand = new Random(System.currentTimeMillis());
 
         for(int i = 0; i < 7; i++) {
             System.out.println(rand.nextInt(1000));     // 0 이상 1000 미만 난수 생성
