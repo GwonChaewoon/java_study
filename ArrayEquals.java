639f7c04a5704b2663e59ef6e1d264faca12b844
chae woon <javaall123@naver.com>
2022-02-16 15:57:58 +0900

    Object 클래스에 정의된 equals 메소드 "참조 값이 아닌 참조하는 인스턴스의 내용을 비교한다."

--- ArrayEquals.java
+++ ArrayEquals.java
@@ -1,9 +1,19 @@
 import java.util.Arrays;
 
+class INum {
+    private int num;
+    public INum(int num) {
+        this.num = num;
+    }
+}
+
 public class ArrayEquals {
     public static void main(String[] args) {
-        int[] ar1 = {1, 2, 3, 4, 5};
-        int[] ar2 = Arrays.copyOf(ar1, ar1.length);
+        INum[] ar1 = new INum[3];
+        INum[] ar2 = new INum[3];
+        ar1[0] = new INum(1);  ar2[0] = new INum(1);
+        ar1[1] = new INum(2);  ar2[1] = new INum(2);
+        ar1[2] = new INum(3);  ar2[2] = new INum(3);
         System.out.println(Arrays.equals(ar1, ar2));
     }
 }
