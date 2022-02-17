0feeee83ed8ddfede972fe855717b947215cabbd
chae woon <javaall123@naver.com>
2022-02-17 17:59:42 +0900

    두 개의 Box를 클래스 하나로 대체

--- FruitAndBox.java
+++ FruitAndBox.java
@@ -10,38 +10,28 @@ class Orange {    // 오렌지를 단순히 표현한 클래스
     }
 }
 
-class AppleBox {    // 사과 담는 상자를 표현한 클래스
-    private Apple ap;
+class Box {      // 무엇이든 담을 수 있는 상자
+    private Object ob;
 
-    public void set(Apple a) {     // 사과를 담는다.
-        ap = a;
+    public void set(Object o) {
+        ob = o;
     }
-    public Apple get() {    // 사과를 꺼낸다.
-        return ap;
-    }
-}
 
-class OrangeBox {   // 오렌지 담는 상자를 표현한 클래스
-    private Orange or;
-
-    public void set(Orange o) {     // 오렌지를 담는다.
-        or = o;
-    }
-    public Orange get() {   // 오렌지를 꺼낸다.
-        return or;
+    public Object get() {
+        return ob;
     }
 }
 
 public class FruitAndBox {
     public static void main(String[] args) {
-        AppleBox aBox = new AppleBox();    // 사과 상자 생성
-        OrangeBox oBox = new OrangeBox();  // 오렌지 상자 생성
+        Box aBox = new Box();  // 상자 생성
+        Box oBox = new Box();  // 상자 생성
 
-        aBox.set(new Apple());     // 사과를 사과 상자에 담는다.
-        oBox.set(new Orange());    // 오렌지를 오렌지 상자에 담는다.
+        aBox.set(new Apple());    // 상자에 사과를 담는다.
+        oBox.set(new Orange());    // 상자에 오렌지를 담는다.
 
-        Apple ap = aBox.get();     // 상자에서 사과를 꺼낸다.
-        Orange og = oBox.get();     // 상자에서 사과를 꺼낸다.
+        Apple ap = (Apple)aBox.get();     // 상자에서 사과를 꺼낸다.
+        Orange og = (Orange)oBox.get();     // 상자에서 오렌지를 꺼낸다.
 
         System.out.println(ap);
         System.out.println(og);
