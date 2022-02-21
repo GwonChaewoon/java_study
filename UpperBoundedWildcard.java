4838eb157f429680e446880534eaa96152781083
chae woon <javaall123@naver.com>
2022-02-21 11:23:18 +0900

    와일드카드의 상한과 하한의 제한: Bounded Wildcards(2) Box<Integer>, Box<Number>, Box<Object>

--- UpperBoundedWildcard.java
+++ UpperBoundedWildcard.java
@@ -10,20 +10,24 @@ class Box<T> {
 }
 
 class Unboxer {
-    public static void peekBox(Box<? extends Number> box) {
+    public static void peekBox(Box<? super Integer> box) {
         System.out.println(box);
     }
 }
 
 public class UpperBoundedWildcard {
     public static void main(String[] args) {
-        Box<Integer> iBox = new Box<>();
-        iBox.set(1234);
+        Box<Integer> iBox = new Box<Integer>();
+        iBox.set(5577);
 
-        Box<Double> dBox = new Box<>();
-        dBox.set(10.009);
+        Box<Number> nBox = new Box<Number>();
+        nBox.set(new Integer(9955));
+
+        Box<Object> oBox = new Box<Object>();
+        oBox.set("My Simple Instance");
 
         Unboxer.peekBox(iBox);
-        Unboxer.peekBox(dBox);
+        Unboxer.peekBox(nBox);
+        Unboxer.peekBox(oBox);
     }
 }
