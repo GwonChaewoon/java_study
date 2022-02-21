33d1abf895fb6678fd72295392a1b09e3a8c21e5
chae woon <javaall123@naver.com>
2022-02-21 11:06:59 +0900

    와일드카드(Whildcard) (2)

--- WildcardUnboxer.java
+++ WildcardUnboxer.java
@@ -13,9 +13,7 @@ class Unboxer {
     public static <T> T openBox(Box<T> box) {
         return box.get();
     }
-
-    // 상자 안의 내용물을 확인하는(출력하는) 기능의 제네릭 메소드
-    public static <T> void peekBox(Box<T> box) {
+    public static <T> void peekBox(Box<?> box) {   // 와일드카드 사용
         System.out.println(box);
     }
 }
@@ -24,6 +22,6 @@ public class WildcardUnboxer {
     public static void main(String[] args) {
         Box<String> box = new Box<>();
         box.set("So Simple String");
-        Unboxer.peekBox(box);   // 상자 안의 내용물을 확인해본다.
+        Unboxer.peekBox(box);
     }
 }
