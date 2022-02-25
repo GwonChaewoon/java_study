58ec8249bcaef0bf1679b20a2e61bd1f93f283b2
chae woon <javaall123@naver.com>
2022-02-25 10:21:33 +0900

    찾기

---
+++ StringBinarySearch.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StringBinarySearch {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Box");
+        list.add("Robot");
+        list.add("Apple");
+
+        Collections.sort(list);    // 정렬
+        int idx = Collections.binarySearch(list, "Robot");    // 탐색
+        System.out.println(list.get(idx));
+    }
+}
