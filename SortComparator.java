ef4714d04657feea508755c45d3b149b576b30bb
chae woon <javaall123@naver.com>
2022-02-27 11:42:02 +0900

    익명 클래스 (Anonymous Class) (3)

--- SortComparator.java
+++ SortComparator.java
@@ -3,13 +3,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Collections;
 
-class StrComp implements Comparator<String> {
-    @Override
-    public int compare(String s1, String s2) {
-        return s1.length() - s2.length();
-    }
-}
-
 public class SortComparator {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
@@ -17,8 +10,14 @@ public class SortComparator {
         list.add("APPLE");
         list.add("Box");
 
-        StrComp cmp = new StrComp();   // 정렬 기준
-        Collections.sort(list, cmp);   // 정렬 기준 변경해서 정렬 진행
+        Comparator<String> cmp = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length();
+            }
+        };
+
+        Collections.sort(list, cmp);
         System.out.println(list);
     }
 }
