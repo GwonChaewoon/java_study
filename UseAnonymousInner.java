d32fa1ba38e6ae9f7b00c36b058165e337b49c46
chae woon <javaall123@naver.com>
2022-02-27 11:33:20 +0900

    익명 클래스 (Anonymous Class)

--- UseAnonymousInner.java
+++ UseAnonymousInner.java
@@ -7,17 +7,15 @@ class Papers {
     public Papers(String s) { con = s; }
 
     public Printable getPrinter() {
-        class Printer implements Printable {  // 로컬 클래스의 정의
+        return new Printable() {   // 익명 클래스의 정의와 인스턴스 생성
             public void print() {
                 System.out.println(con);
             }
-        }
-
-        return new Printer();    // 로컬 클래스의 인스턴스 생성 및 반환
+        };
     }
 }
 
-public class UseLocalInner {
+public class UseAnonymousInner {
     public static void main(String[] args) {
         Papers p = new Papers("서류 내용: 행복합니다.");
         Printable prn = p.getPrinter();
