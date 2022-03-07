f993b5c894980415fa404440d85dfa2815fc5e1a
chae woon <javaall123@naver.com>
2022-03-07 20:20:15 +0900

    람다의 이해(2) '익명 클래스' 적용

--- Lamda1.java
+++ Lamda1.java
@@ -2,15 +2,14 @@ interface Printable {
     void print(String s);
 }
 
-class Printer implements Printable {
-    public void print(String s) {
-        System.out.println(s);
-    }
-}
-
 public class Lamda1 {
     public static void main(String[] args) {
-        Printable prn = new Printer();
+        Printable prn = new Printable() {
+            public void print(String s) {   // 익명 클래스
+                System.out.println(s);
+            }
+        };
+
         prn.print("What is Lambda?");
     }
 }
