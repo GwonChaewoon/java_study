620fcda30080c55a357428126a0825d229ef0aed
chae woon <javaall123@naver.com>
2022-03-07 20:24:22 +0900

    람다의 이해(3) '람다식(Lambda Expression)' 사용

--- Lamda1.java
+++ Lamda1.java
@@ -4,12 +4,7 @@ interface Printable {
 
 public class Lamda1 {
     public static void main(String[] args) {
-        Printable prn = new Printable() {
-            public void print(String s) {   // 익명 클래스
-                System.out.println(s);
-            }
-        };
-
+        Printable prn = (s) -> { System.out.println(s); };
         prn.print("What is Lambda?");
     }
 }
