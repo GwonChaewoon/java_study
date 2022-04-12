813a5d323a0439850daff52715c700111ed8a886
chae woon <javaall123@naver.com>
2022-04-12 15:44:59 +0900

    static 메소드의 참조(2)

--- ArrangeList2.java
+++ ArrangeList2.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.function.Consumer;
 
-public class ArrangeList {
+public class ArrangeList2 {
     public static void main(String[] args) {
         List<Integer> ls = Arrays.asList(1, 3, 5, 7, 9);
         ls = new ArrayList<>(ls);
 
-        Consumer<List<Integer>> c = l -> Collections.reverse(l);   // 람다식
-        c.accept(ls);
+        Consumer<List<Integer>> c = Collections::reverse;  // 메소드 참조
+        c.accept(ls);   // 전달 인자 ls를 reverse에 그대로 전달하게 된다.
         System.out.println(ls);
-    }
+    }   
 }
