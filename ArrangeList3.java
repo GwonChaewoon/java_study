6e274614cd3fc86c5752689fa4f070161108690a
chae woon <javaall123@naver.com>
2022-04-12 15:51:27 +0900

    인스턴스 메소드의 참조 1: 인스턴스가 존재하는 상황에서 참조

--- ArrangeList3.java
+++ ArrangeList3.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.function.Consumer;
 
-public class ArrangeList2 {
+class JustSort {
+    public void sort(List<?> lst) {     // 인스턴스 메소드
+        Collections.reverse(lst);
+    }
+}
+
+public class ArrangeList3 {
     public static void main(String[] args) {
         List<Integer> ls = Arrays.asList(1, 3, 5, 7, 9);
         ls = new ArrayList<>(ls);
+        JustSort js = new JustSort();
 
-        Consumer<List<Integer>> c = Collections::reverse;  // 메소드 참조
-        c.accept(ls);   // 전달 인자 ls를 reverse에 그대로 전달하게 된다.
+        Consumer<List<Integer>> c = e -> js.sort(e);   // 람다식 기반
+        c.accept(ls);
         System.out.println(ls);
-    }   
+    }
 }
